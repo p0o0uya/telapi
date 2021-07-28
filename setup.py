691f2ef3f7d0841b5cd35cc70a5c2934d48079b8
p0o0uya <pouyajamali@gmail.com>
2021-07-29 02:17:19 +0430

    renaming module to be accepted on PyPi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='telapi',
+    name='telegapi',
     version='0.0',
     author='Pouya Jamali',
     author_email='pouyajamali@gmail.com',
--- telapi/__init__.py
+++
@@ -1,2 +0,0 @@
-from telapi.reqs import reqs as reqs
-from telapi.telapi import telapi
