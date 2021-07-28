784328bdf693ea3ca36b5baf6ab0a3936310eeac
p0o0uya <pouyajamali@gmail.com>
2021-07-29 01:03:39 +0430

    gitignore updated together with setup files

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='telapi',
+    version='0.0',
+    author='Pouya Jamali',
+    author_email='pouyajamali@gmail.com',
+    description='A wrapper for telegram bot API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/p0o0uya/telapi',
+    project_urls = {
+        "Bug Tracker": "https://github.com/p0o0uya/telapi/issues"
+    },
+    license='MIT',
+    packages=['telapi'],
+    install_requires=['requests', 'pandas'],
+)
